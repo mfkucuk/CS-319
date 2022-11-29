a9b60e470cba5c9c43af9183ef7d3093334f7497
mfkucuk <mfkucuk555@gmail.com>
2022-11-29 20:07:14 +0300

    React & Spring Boot Setup

---
+++ erasmuspp/erasmusppspringboot/src/main/java/com/erasmuspp/erasmusppspringboot/ErasmusppspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.erasmuspp.erasmusppspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusppspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmusppspringbootApplication.class, args);
+	}
+
+}
