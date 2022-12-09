851778a6cad6e59d28f02c19f2ff591a937dd547
Apu322 <75177705+Apu322@users.noreply.github.com>
2022-12-09 21:58:26 +0300

    PostgreSQL integration and Announcement Support

---
+++ erasmuspp/erasmusppspringboot/src/main/java/com/erasmuspp/erasmusppspringboot/model/Announcement.java
@@ -0,0 +1,50 @@
+package com.erasmuspp.erasmusppspringboot.model;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Announcement 
+{
+    private final UUID id;
+    private final String title;
+    private final String content;
+    private final String postDate;
+    private final String expireDate;
+
+    public Announcement(@JsonProperty("id") UUID id,
+                        @JsonProperty("title") String title,
+                        @JsonProperty("content") String content,
+                        @JsonProperty("postDate") String postDate,
+                        @JsonProperty("expireDate") String expireDate)
+                        {
+                            this.id = id;
+                            this.title = title;
+                            this.content = content;
+                            this.postDate = postDate;
+                            this.expireDate = expireDate;
+                        }
+
+    public UUID getId()
+    {
+        return id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getContent()
+    {
+        return content;
+    }
+    public String getPostDate()
+    {
+        return postDate;
+    }
+    public String getExpireDate()
+    {
+        return expireDate;
+    }
+}
