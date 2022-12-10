ca2c8db699258f3e06ccf9e9d4fcb03812bfceb1
Ege Ayan <ayanege2001@gmail.com>
2022-12-10 18:09:37 +0300

    login screen

--- erasmuspp/erasmuspp-react/src/App.js
+++ erasmuspp/erasmuspp-react/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import Alert from 'react-bootstrap/Alert';
 import './App.css';
+import LoginScreen from './components/LoginScreen';
 
 function App() {
-  return (<>
-    {[
-      'primary',
-      'secondary',
-      'success',
-      'danger',
-      'warning',
-      'info',
-      'light',
-      'dark',
-    ].map((variant) => (
-      <Alert key={variant} variant={variant}>
-        This is a {variant} alert—check it out!
-      </Alert>
-    ))}
-  </>
-   
+  return (
+    <>
+      <LoginScreen> 
+      </LoginScreen> 
+    
+    
+    
+    </>
   );
 }
 
