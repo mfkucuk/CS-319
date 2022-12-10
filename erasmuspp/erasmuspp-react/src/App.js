5e2b3e2781dea3ea09f9c989022b59f275f9f841
Ege Ayan <ayanege2001@gmail.com>
2022-12-10 17:16:38 +0300

    bootstrap

--- erasmuspp/erasmuspp-react/src/App.js
+++ erasmuspp/erasmuspp-react/src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
+import Alert from 'react-bootstrap/Alert';
 import './App.css';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <h1> Hello </h1>
     </div>
+=======
+    <>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </>
+>>>>>>> Stashed changes
   );
 }
 
