10030b421123d772df0d4047be19256acf82b4f0
Ledauphin <yerent2000@gmail.com>
2022-12-14 00:45:31 +0300

    My Application version 1
    button added but not in a good format

--- erasmuspp/erasmuspp-react/src/components/UploadFileModalPopUp.js
+++ erasmuspp/erasmuspp-react/src/components/UploadFileModalPopUp.js
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-export default function SaveCandidateInfoModalPopUpY() {
+export default function UploadFileModalPopUpY() {
   const [show, setShow] = useState(false);
+  const [buttonIndex, changeButtonIndex] = useState(0);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  let buttonName;
+
+  if (buttonIndex == 0){
+    buttonName = "Upload Statement of Purpose"
+  }
   return (
     <>
       <Button style={{backgroundColor: "#3C7479"}} onClick={handleShow}>
-        Save Candidate Info
+        {buttonName}
       </Button>
 
       <Modal show={show} onHide={handleClose}>
