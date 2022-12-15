f99b3c5502fe25cf48e9d5d2ca439de2a0fee8b5
enderutlu <ender.utlu@ug.bilkent.edu.tr>
2022-12-16 00:47:36 +0300

    Forgot Password completed

--- erasmuspp/erasmuspp-react/src/App.js
+++ erasmuspp/erasmuspp-react/src/App.js
@@ -14,6 +14,7 @@ import MyApplicationY from './components/MyApplicationY';
 import ApplicationFormEditY from './components/ApplicationFormEditY';
 import ApplicationFormViewY from './components/ApplicationFormViewY';
 import ToDoListN from './components/ToDoListN';
+import ForgotPasswordN from './components/ForgotPasswordN';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
         <Route path ="/editApplicationForm" element = {<ApplicationFormEditY/>}> </Route>
         <Route path ="/viewApplicationForm" element = {<ApplicationFormViewY/>}> </Route>
         <Route path ="/toDoList" element = {<ToDoListN/>}> </Route>
+        <Route path ="/forgotPassword" element = {<ForgotPasswordN/>}> </Route>
       </Routes>  
     </>
   );
