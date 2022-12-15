da527ff46947428f4d0a19d3ddaad8ce3a150cbf
Ege Ayan <ayanege2001@gmail.com>
2022-12-15 22:57:10 +0300

    base64

--- erasmuspp/erasmuspp-react/src/components/MyApplicationY.js
+++ erasmuspp/erasmuspp-react/src/components/MyApplicationY.js
@@ -1,12 +1,16 @@
 import Form from 'react-bootstrap/Form';
-import React, { useState } from 'react';
 import TopNavBar from './TopNavBar';
 import DefaultFooter from './DefaultFooter';
 import UploadFileModalPopUpY from './UploadFileModalPopUp';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function MyApplicationY(){
+  const [statementOfPurpose, setStatementOfPurpose]= useState("");
+  const [sopB64, setsopB64] = useState("");
+
   let navigate = useNavigate();
   function handleClickEditApplicationForm() {
       navigate("/editApplicationForm");
@@ -14,6 +18,31 @@ export default function MyApplicationY(){
 
   function handleClickViewApplicationForm() {
     navigate("/viewApplicationForm");
+  }
+
+  const uploadStatemenetOfPurpose = async (e) => {
+    const statementOfPurpose = e.target.files[0];
+    
+    setsopB64(await sopToB64(statementOfPurpose));
+  };
+
+  const sopToB64 = (file) => {
+      return new Promise ((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+
+    })
+
+  }
+
+  const uploadSOPFinal = ()=> { 
+    console.log(sopB64);
   }
 
   return(
@@ -36,7 +65,7 @@ export default function MyApplicationY(){
           <div class="col-md-3" style={{backgroundColor: "#1F8F8E"}}>
             <Form.Group controlId="uploadStatementofPurposeForm" className="mb-3">
               <Form.Label style={{color: '#f4eff2'}}>Upload Statement of Purpose</Form.Label>
-              <Form.Control type="file" size="lg" />
+              <Form.Control type="file" size="lg" value = {statementOfPurpose} onChange = {(e) =>  {setStatementOfPurpose(e.target.value); uploadStatemenetOfPurpose(e); }} />
             </Form.Group>
             <br/>
             <Form.Group controlId="uploadCVForm" className="mb-3">
@@ -61,11 +90,23 @@ export default function MyApplicationY(){
           </div>
           <div class="col-md-1" style={{backgroundColor: "#1F8F8E"}}>
             <br/>
-            <UploadFileModalPopUpY/>
+            <div className='pt-2'>
+            <Button style={{backgroundColor: "#3C7479"}} onClick={uploadSOPFinal}>
+            Upload
+            </Button>
+            </div>
             <br/><br/><br/>
-            <UploadFileModalPopUpY/>
+            <div className='pt-2'>
+            <Button style={{backgroundColor: "#3C7479"}}>
+            Upload
+            </Button>
+            </div>
             <br/><br/><br/>
-            <UploadFileModalPopUpY/>             
+            <div className='pt-2'>
+            <Button style={{backgroundColor: "#3C7479"}}>
+            Upload
+            </Button>
+            </div>             
           </div>
           <div class="col-md-3 text-center" style={{backgroundColor: "#1F8F8E"}}>
 
