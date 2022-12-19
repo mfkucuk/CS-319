cbe263030925c078381f2cb9d2c863af863ae737
Ledauphin <yerent2000@gmail.com>
2022-12-19 05:17:58 +0300

    roadmap changes

--- erasmuspp/erasmuspp-react/src/components/RoadmapY.js
+++ erasmuspp/erasmuspp-react/src/components/RoadmapY.js
@@ -74,10 +74,14 @@ export default function RoadmapY() {
               <div class="col-md-10 text-center" style={{ backgroundColor: "#1F8F8E" }}>
                 <br /><br /><br /><br /><br />
                 <ProgressBar style={{ height: "2rem" }}>
-                  <ProgressBar striped style={{ backgroundColor: "#76AA5F" }} animated now={stageOneWeight} key={1} label={"Application"} />
+                  {/* <ProgressBar striped style={{ backgroundColor: "#76AA5F" }} animated now={stageOneWeight} key={1} label={"Application"} />
                   <ProgressBar striped style={{ backgroundColor: "#54363D" }} animated now={stageTwoWeight} key={2} label={"Course Registration"} />
                   <ProgressBar striped style={{ backgroundColor: "#266A80" }} animated now={stageThreeWeight} key={3} label={"Pre-Approval Form"} />
-                  <ProgressBar striped style={{ backgroundColor: "#385A4E" }} animated now={stageFourWeight} key={4} label={"Learning Agreement"} />
+                  <ProgressBar striped style={{ backgroundColor: "#385A4E" }} animated now={stageFourWeight} key={4} label={"Learning Agreement"} /> */}
+                  <ProgressBar striped style={{ backgroundColor: "#76AA5F" }} animated now={25} key={1} label={"Application"} />
+                  <ProgressBar striped style={{ backgroundColor: "#54363D" }} animated now={25} key={2} label={"Course Registration"} />
+                  <ProgressBar striped style={{ backgroundColor: "#266A80" }} animated now={25} key={3} label={"Pre-Approval Form"} />
+                  <ProgressBar striped style={{ backgroundColor: "#385A4E" }} animated now={25} key={4} label={"Learning Agreement"} />
                 </ProgressBar>
               </div>
               <div class="col-md-1 text-center" style={{ backgroundColor: "#1F8F8E" }}>
