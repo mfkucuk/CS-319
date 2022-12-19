725942db9471f5b2726d41a51ddd9acc62c96327
Ege Ayan <ayanege2001@gmail.com>
2022-12-19 03:14:30 +0300

    defaultpp added

--- erasmuspp/erasmuspp-react/src/components/ProfileScreen.jsx
+++ erasmuspp/erasmuspp-react/src/components/ProfileScreen.jsx
@@ -6,6 +6,7 @@ import LargeBreak from './LargeBreak';
 import TopNavBar from './TopNavBar';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
+import Image from "./defaultpp.png";
 
 export default function ProfileScreen() {
   const [userFirstNameInit, setUserFirstNameInit] = useState("");
@@ -22,7 +23,7 @@ export default function ProfileScreen() {
   const [userAboutMe, setUserAboutMe] = useState("");
   const [userAboutMeInit, setUserAboutMeInit] = useState("");
 
-  const [userImageSrcInit, setUserImageSrcInit] = useState("");
+  const [userImageSrcInit, setUserImageSrcInit] = useState(Image);
 
   const[userPP, setuserPP] = useState("");
   const[userPPB64, setuserPPB64] = useState("");
